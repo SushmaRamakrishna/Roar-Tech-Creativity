2e8d67fb10ab922dbca4eff894c7ca9ae63ec67b
SushmaRamakrishna <urpalshu@gmail.com>
2020-06-21 17:08:47 -0500

    added redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import "./collection-item.styles.scss";
+import './collection-item.styles.scss';
 
-const CollectionItems = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className='collection-item'>
+    <div
+      className='image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
   </div>
 );
 
-export default CollectionItems;
+export default CollectionItem;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,27 +1,49 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyAUCR105gXu8kxz_J9sJahaSBv0Qpb2aT0",
-  authDomain: "roar-tech-db.firebaseapp.com",
-  databaseURL: "https://roar-tech-db.firebaseio.com",
-  projectId: "roar-tech-db",
-  storageBucket: "roar-tech-db.appspot.com",
-  messagingSenderId: "799975663490",
-  appId: "1:799975663490:web:72f6d14d7297699e8a919b",
-  measurementId: "G-MGDGGHXF9X",
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
 
-export const auth = firebase.auth();
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GithubAuthProvider();
-
-provider.setCustomParameters({ promt: "select_account" });
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
